8446123a5805843aab1bfb1e0d677e54fd381353
highonlinux420 <adelbourouis9@gmail.com>
2021-01-18 14:49:26 +0100

    Ability to upgrade

--- YAPY.py
+++ YAPY.py
@@ -9,41 +9,14 @@ import requests
 from termcolor import colored
 
 parser = argparse.ArgumentParser(description="'YAPY', Yet Another Python Yaourt :P")
-parser.add_argument("package", type=str, help="Package to install")
+parser.add_argument("-S", help="upgrade AUR packages", action="store_true")
+parser.add_argument("-I", type=str, help="package to install")
 args = parser.parse_args()
 
 check_OS = subprocess.run("cat /etc/arch-release", shell=True, capture_output=True)
 if check_OS.returncode != 0:
     print("Not arch-based system, Quitting....")
     sys.exit()
-
-def aur_response():
-    if len(args.package) > 1:
-        try:
-            response = requests.get(f"https://aur.archlinux.org//rpc/?v=5&type=search&by=name-desc&arg={args.package}")
-            results_AUR = response.json().get("results")
-            return results_AUR
-        except:
-            print("Could not search for packages, check your network")
-            sys.exit()
-    else:
-        print("Package name is too short")
-        sys.exit()
-
-
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    future = executor.submit(aur_response)
-    results_AUR = future.result()
-
-response = requests.get(f"https://www.archlinux.org/packages/search/json/?q={args.package}")
-results_REPO = response.json().get("results")
-if len(results_AUR) != 0 or len(results_REPO) != 0:
-    print(len(results_AUR), "packages found in AUR")
-    print(len(results_REPO), "packages found in repos")
-else:
-    print("No packages found, Quitting...")
-    sys.exit()
-
 
 class IterPackage(type):
     def __iter__(cls):
@@ -58,7 +31,6 @@ class Package(metaclass=IterPackage):
         self.ID = ID
         self.Name = Name
         self.Origin = Origin
-
 
 def get_pack(array, org, name, ver, desc, repo, k):
     j = k + 10
@@ -99,41 +71,7 @@ def get_pack(array, org, name, ver, desc, repo, k):
         sys.exit()
 
 
-while True:
-    try:
-        if len(results_AUR) != 0 and len(results_REPO) != 0:
-            choice = input("Use AUR or official repos ? [AUR, OFF]: ").upper()
-            if choice in ["AUR", "OFF"]:
-                break
-        elif len(results_AUR) == 0:
-            subprocess.run('read -s -n 1 -p "Indexing Official repos, Press any key...."', shell=True)
-            choice = "OFF"
-            print("\n")
-            break
-        else:
-            subprocess.run('read -s -n 1 -p "Indexing AUR repo, Press any key...."', shell=True)
-            choice = "AUR"
-            print("\n")
-            break
-    except KeyboardInterrupt:
-        sys.exit()
-if choice == "OFF":
-    package = get_pack(results_REPO, "OFF", "pkgname", "pkgver", "pkgdesc", "repo", 0)
-    try:
-        sudo = getpass()
-    except KeyboardInterrupt:
-        sys.exit()
-    install = subprocess.run(f"echo {sudo} | sudo -S pacman --noconfirm -S {package}", shell=True, text=True,
-                             stderr=subprocess.PIPE)
-    if install.returncode != 0:
-        try:
-            response = requests.get("https://www.archlinux.org")
-        except:
-            print("No network, Quitting...")
-            sys.exit()
-        print("Password is incorrect")
-elif choice == "AUR":
-    package = get_pack(results_AUR, "AUR", "Name", "Version", "Description", "Maintainer", 0)
+def install_aur(package):
     subprocess.run(f"mkdir {package} && cd {package}", shell=True, capture_output=True)
     wget = subprocess.run(
         f"wget -O {package}/PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h={package}",
@@ -164,6 +102,84 @@ elif choice == "AUR":
                     break
         build = subprocess.run(f"cd {package} && makepkg -s", shell=True)
         if build.returncode == 0:
-            build = subprocess.run(f"cd {package} && makepkg --install", shell=True)
+            subprocess.run(f"cd {package} && makepkg --install", shell=True)
     else:
         print(wget.stderr)
+
+def aur_response():
+    if len(args.I) > 1:
+        try:
+            response = requests.get(f"https://aur.archlinux.org//rpc/?v=5&type=search&by=name-desc&arg={args.I}")
+            results_AUR = response.json().get("results")
+            return results_AUR
+        except:
+            print("Could not search for packages, check your network")
+            sys.exit()
+    else:
+        print("Package name is too short")
+        sys.exit()
+
+if args.S:
+    print("Checking out of date AUR packages...")
+    outdated = subprocess.run("pacman -Qm", shell=True, text=True, capture_output=True)
+    j = 0
+    for i in range(len(outdated.stdout.splitlines())):
+        response = requests.get(f"https://aur.archlinux.org/rpc/?v=5&type=info&arg[]={outdated.stdout.splitlines()[i].split(' ')[0]}")
+        if response.json().get("results")[0].get("Version") != outdated.stdout.splitlines()[i].split(' ')[1]:
+            j += 1
+            install_aur(outdated.stdout.splitlines()[i].split(" ")[0])
+    if j == 0:
+        print("All packages are up-to-date")
+    else:
+        print(f"{j} packages upgraded")
+elif args.I != None:
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(aur_response)
+        results_AUR = future.result()
+
+    response = requests.get(f"https://www.archlinux.org/packages/search/json/?q={args.I}")
+    results_REPO = response.json().get("results")
+    if len(results_AUR) != 0 or len(results_REPO) != 0:
+        print(len(results_AUR), "packages found in AUR")
+        print(len(results_REPO), "packages found in repos")
+    else:
+        print("No packages found, Quitting...")
+        sys.exit()
+    while True:
+        try:
+            if len(results_AUR) != 0 and len(results_REPO) != 0:
+                choice = input("Use AUR or official repos ? [AUR, OFF]: ").upper()
+                if choice in ["AUR", "OFF"]:
+                    break
+            elif len(results_AUR) == 0:
+                subprocess.run('read -s -n 1 -p "Indexing Official repos, Press any key...."', shell=True)
+                choice = "OFF"
+                print("\n")
+                break
+            else:
+                subprocess.run('read -s -n 1 -p "Indexing AUR repo, Press any key...."', shell=True)
+                choice = "AUR"
+                print("\n")
+                break
+        except KeyboardInterrupt:
+            sys.exit()
+    if choice == "OFF":
+        package = get_pack(results_REPO, "OFF", "pkgname", "pkgver", "pkgdesc", "repo", 0)
+        try:
+            sudo = getpass()
+        except KeyboardInterrupt:
+            sys.exit()
+        install = subprocess.run(f"echo {sudo} | sudo -S pacman --noconfirm -S {package}", shell=True, text=True,
+                                 stderr=subprocess.PIPE)
+        if install.returncode != 0:
+            try:
+                response = requests.get("https://www.archlinux.org")
+            except:
+                print("No network, Quitting...")
+                sys.exit()
+            print("Password is incorrect")
+    elif choice == "AUR":
+        package = get_pack(results_AUR, "AUR", "Name", "Version", "Description", "Maintainer", 0)
+        install_aur(package=package)
+else:
+    print("Type yapy -h for usage")
